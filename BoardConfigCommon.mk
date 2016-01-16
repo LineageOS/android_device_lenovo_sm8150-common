df4c1456a68abc636f10973804aa05a064ce3161
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    sm8150-common: Clean up kernel cmdline
     * Console is overwritten by the bootloader
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
       and the same goes for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,14 +36,11 @@ BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.boot_devices=soc/1d84000.ufshc \
-    androidboot.console=ttyMSM0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
-    console=ttyMSM0,115200n8 \
     earlycon=msm_geni_serial,0xa90000 \
     loop.max_part=7 \
-    msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=2048 \
     video=vfb:640x400,bpp=32,memsize=3072000
