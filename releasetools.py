9d06fbac65b751c3ce8217026f7299ff5d8d33ae
dianlujitao <dianlujitao@lineageos.org>
2017-07-15 16:37:47 +0200

    sm8150-common: Fix modem time-stamp regex
    Change-Id: I964d4effbdf6af778361ef79cb4598e24a46fe8b

--- releasetools.py
+++ releasetools.py
@@ -37,11 +37,11 @@ def IncrementalOTA_InstallEnd(info):
 
 def AddModemAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(\S+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=(.+)', android_info)
   if m:
     versions = m.group(1).split('|')
     if len(versions) and '*' not in versions:
-      cmd = 'assert(lenovo.verify_modem(' + ','.join(['"%s"' % modem for modem in versions]) + ') == "1");'
+      cmd = 'assert(lenovo.verify_modem(' + ','.join(['"%s"' % modem.strip() for modem in versions]) + ') == "1");'
       info.script.AppendExtra(cmd)
 
 def AddImage(info, basename, dest):
