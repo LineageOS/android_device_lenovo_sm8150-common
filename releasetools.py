aaf804e72ec33b2467ea7cc73079d2b074d12bba
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-05-28 20:56:14 +0200

    sm8150-common: releasetools: Make modem check more user friendly
    Change-Id: Ib334abf1d53ba889d1b4f746657336fae2d47db2

--- releasetools.py
+++ releasetools.py
@@ -39,10 +39,11 @@ def AddModemAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   if m:
-    version = m.group(1).rstrip()
-    if len(version) and '*' not in version:
-      cmd = 'assert(lenovo.verify_modem("' + version + '") == "1");'
-      info.script.AppendExtra(cmd)
+    timestamp, firmware_version = m.group(1).rstrip().split(',')
+    if ((len(timestamp) and '*' not in timestamp) and \
+        (len(firmware_version) and '*' not in firmware_version)):
+      cmd = 'assert(lenovo.verify_modem("{}") == "1" || abort("ERROR: This package requires firmware from ZUI {} build or newer. Please upgrade firmware and retry!"););'
+      info.script.AppendExtra(cmd.format(timestamp, firmware_version))
 
 def AddImage(info, basename, dest):
   name = basename
