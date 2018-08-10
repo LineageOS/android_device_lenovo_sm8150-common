7fbfce5857867d847524b48f9ec6dd45abf6c94a
YumeMichi <do4suki@gmail.com>
2018-08-10 04:15:00 +0200

    nx611j: update recovery_updater for P
    Change-Id: I28d1133fcee0ea8010e568fda8ba6bdebb64e338
    (cherry picked from commit 6b1940945824fca0cd075e3a525a7cbc76660b75)

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -4,7 +4,9 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
     bootable/recovery \
     bootable/recovery/edify/include \
-    bootable/recovery/otautil/include
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include
+
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_lenovo
 
