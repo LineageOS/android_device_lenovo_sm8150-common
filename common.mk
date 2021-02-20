6cfb6e8bc398b686eade5fdfce33aaee457f5e22
Lucchetto <lucchetto.tie@live.com>
2021-02-20 19:54:51 +0100

    sm8150-common: build libavservices_minijail.vendor
        * Required by new IMS stack
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Ia8fbef359c0d9e47371d62adc56da06c41c526bf

--- common.mk
+++ common.mk
@@ -263,6 +263,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail_vendor \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
