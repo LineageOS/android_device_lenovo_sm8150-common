karin722/device_oneplus_guacamoleb

sm8150-common: fod: prettify soong variables

BoardConfigCommon.mk
@@ -190,5 +190,5 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Fingerprint
-SOONG_CONFIG_NAMESPACES += oneplusMsmnileFodPlugin
-SOONG_CONFIG_oneplusMsmnileFodPlugin := posX posY size
+SOONG_CONFIG_NAMESPACES += ONEPLUS_MSMNILE_FOD
+SOONG_CONFIG_ONEPLUS_MSMNILE_FOD := POS_X POS_Y SIZE

soong/fod.go
@@ -9,10 +9,10 @@ import (
 func deviceFlags(ctx android.BaseContext) []string {
     var cflags []string
 
-    var config = ctx.AConfig().VendorConfig("oneplusMsmnileFodPlugin")
-    var posX = strings.TrimSpace(config.String("posX"))
-    var posY = strings.TrimSpace(config.String("posY"))
-    var size = strings.TrimSpace(config.String("size"))
+    var config = ctx.AConfig().VendorConfig("ONEPLUS_MSMNILE_FOD")
+    var posX = strings.TrimSpace(config.String("POS_X"))
+    var posY = strings.TrimSpace(config.String("POS_Y"))
+    var size = strings.TrimSpace(config.String("SIZE"))
 
     cflags = append(cflags, "-DFOD_POS_X=" + posX, "-DFOD_POS_Y=" + posY, "-DFOD_SIZE=" + size)
     return cflags

LineageOS/android_device_lenovo_sm8150-common

Change-Id: I0743e0e3e481e6780894244842face592d21017b

BoardConfigCommon.mk
@@ -118,8 +118,8 @@ PRODUCT_PRODUCT_VNDK_VERSION := current
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH_COMMON)/config.fs
 
 # FOD
-SOONG_CONFIG_NAMESPACES += lenovoMsmnileFodPlugin
-SOONG_CONFIG_lenovoMsmnileFodPlugin := posX posY size
+SOONG_CONFIG_NAMESPACES += LENOVO_MSMNILE_FOD
+SOONG_CONFIG_LENOVO_MSMNILE_FOD := POS_X POS_Y SIZE
 TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH_COMMON):libfod_extension.lenovo_msmnile
 TARGET_USES_FOD_ZPOS := true
 

soong/fod.go
@@ -9,10 +9,10 @@ import (
 func deviceFlags(ctx android.BaseContext) []string {
     var cflags []string
 
-    var config = ctx.AConfig().VendorConfig("lenovoMsmnileFodPlugin")
-    var posX = strings.TrimSpace(config.String("posX"))
-    var posY = strings.TrimSpace(config.String("posY"))
-    var size = strings.TrimSpace(config.String("size"))
+    var config = ctx.AConfig().VendorConfig("LENOVO_MSMNILE_FOD")
+    var posX = strings.TrimSpace(config.String("POS_X"))
+    var posY = strings.TrimSpace(config.String("POS_Y"))
+    var size = strings.TrimSpace(config.String("SIZE"))
 
     cflags = append(cflags, "-DFOD_POS_X=" + posX, "-DFOD_POS_Y=" + posY, "-DFOD_SIZE=" + size)
     return cflags