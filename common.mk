5efb662e07958554da85a3db995bff349721427b
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-19 09:37:49 -0400

    sm8150-common: turn off compressed apex
    Google introduced compressed apex on android 12 [1], and the flag is
    enabled by default in updatable_apex.mk [2]. Asus Zenfone 5z cannot boot
    compressed apex with encryption, so disable it.
    
    ref:
    [1] https://source.android.com/devices/tech/ota/apex#compressed-apex
    [2] https://android.googlesource.com/platform/build/+/4a1240de8a2ee5795a30df123058677d3999145e
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I6b213b5d4a368adcf77cbecc699496285adf184c

--- common.mk
+++ common.mk
@@ -12,6 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
