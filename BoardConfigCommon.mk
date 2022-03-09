LineageOS/android_device_oneplus_sm8150-common

sm8150-common: Use PRODUCT_*_SEPOLICY_DIRS to build product sepolicy.
We are restoring the old behavior of BOARD_PLAT_*_SEPOLICY_DIR that
extends system sepolicy. To that end, we use another make variable to
specify product sepolicy.

Bug: 119305624
Test: m selinux_policy
Change-Id: I67636b7aaff2bd5ae5309981f9fa2f8ce51edf6f

BoardConfigCommon.mk
@@ -144,9 +144,9 @@ VENDOR_SECURITY_PATCH := 2021-12-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/public
 
 # Sensors
 SOONG_CONFIG_NAMESPACES += ONEPLUS_MSMNILE_SENSORS

LineageOS/android_device_lenovo_sm8150-common

BoardConfigCommon.mk
@@ -163,9 +163,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH_COMMON)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
+PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/private
+PRODUCT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true