48a8b5f6c1d482be529a134f152172061843ccca
Lucchetto <lucchetto.tie@live.com>
2022-08-17 19:25:32 +0200

    sm8150-common: releasetools: decode baseband version string
    Change-Id: Id3f4f74ee25ca6fdd4cb102f6f072b0369587507
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- releasetools.py
+++ releasetools.py
@@ -37,7 +37,7 @@ def IncrementalOTA_InstallEnd(info):
 
 def AddModemAssertion(info, input_zip):
   android_info = info.input_zip.read("OTA/android-info.txt")
-  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info.decode('utf-8'))
   if m:
     timestamp, firmware_version = m.group(1).rstrip().split(',')
     if ((len(timestamp) and '*' not in timestamp) and \
