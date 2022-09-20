14a8e78f40515ac5849226c618fe08b26386acaf
Kujou Yuko <i@779.moe>
2022-09-20 13:12:05 +0000

    sm8150-common: Remove duplicate loop.max_part=7 from kernel cmdline
    Change-Id: Ib4632febb5a3bb4c88f5235172f298b2f13f31c6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -40,7 +40,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
     earlycon=msm_geni_serial,0xa90000 \
-    loop.max_part=7 \
     service_locator.enable=1 \
     swiotlb=2048 \
     video=vfb:640x400,bpp=32,memsize=3072000
