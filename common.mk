b207f0caf82fc94780fb0dde22c5931c5d0ef8d6
Ethan Chen <intervigil@gmail.com>
2022-10-29 22:08:16 +0000

    sm8150-common: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- common.mk
+++ common.mk
@@ -350,13 +350,6 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
