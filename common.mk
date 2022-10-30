5e844166db6a9423c21eedcda4be84e095ba7482
dianlujitao <dianlujitao@lineageos.org>
2022-10-30 05:13:43 +0000

    sm8150-common: Remove libbthost_if from product packages
    Change-Id: Iccf81b853ad67c47d23868899bf577ca0a1c6dde

--- common.mk
+++ common.mk
@@ -130,7 +130,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
