230396faaace8c10c68bfb8f5fdfc33c770d03b9
Bruno Martins <bgcngm@gmail.com>
2023-10-24 18:12:15 +0100

    sm8150-common: Migrate to restructured sepolicy_vndr
    The new folder structure now looks like this:
    
      device/qcom/sepolicy_vndr
      ├─ legacy-um (meant for sdm845 until sm8350)
      ├─ sm8450
      └─ sm8550
    
    SEPolicy.mk placed in the root automatically redirects devices to the
    proper subfolder.
    
    Change-Id: I4ed1a06af779909d64172da294c4a99a3ae9ba0d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,8 +161,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH_COMMON)
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
-# Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+# SEPolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH_COMMON)/sepolicy/private
