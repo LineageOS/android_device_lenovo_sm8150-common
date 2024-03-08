6417d518894f407655812413774b5c4d98a76b57
Chaohui Wang <chaohuiw@google.com>
2024-03-08 15:24:40 +0530

    sm8150-common: parts: Migrate to CompoundButton.OnCheckedChangeListener
    Switch and SwitchCompat are both CompoundButton.
    
    Using CompoundButton in Java will helps migration in the future.
    
    Bug: 306658427 | AOSP
    Test: manual - check Settings pages
    Test: m RunSettingsLibRoboTests
    Change-Id: I85a70d4c504d8584030ea4a058f30d74206ab835

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -26,7 +26,8 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Switch;
+import android.widget.CompoundButton;
+
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
@@ -34,12 +35,11 @@ import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.widget.MainSwitchPreference;
-import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import org.lineageos.settings.R;
 
-public class DozeSettingsFragment extends PreferenceFragment implements
-        OnPreferenceChangeListener, OnMainSwitchChangeListener {
+public class DozeSettingsFragment extends PreferenceFragment
+        implements CompoundButton.OnCheckedChangeListener, Preference.OnPreferenceChangeListener {
 
     private MainSwitchPreference mSwitchBar;
 
@@ -121,7 +121,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
     }
 
     @Override
-    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         DozeUtils.enableDoze(getActivity(), isChecked);
         DozeUtils.checkDozeService(getActivity());
 
