c99e453c1e07d2f90ae75df681c338d48f9b4b61
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    sm8150-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- common.mk
+++ common.mk
@@ -9,9 +9,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
 # Setup dalvik vm configs
