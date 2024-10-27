8d946934e0029b70297dee79d8138f5729c18041
Kelvin Zhang <zhangkelvin@google.com>
2024-10-27 16:15:35 +0100

    sm8150-common: Allow to access bootable/deprecated-ota targets
    sm8150-common is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm8150-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- common.mk
+++ common.mk
@@ -398,7 +398,8 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    bootable/deprecated-ota
 
 # Telephony
 PRODUCT_PACKAGES += \
