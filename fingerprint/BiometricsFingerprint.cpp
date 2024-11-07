4bdcdb81b2401f0c30496776b3789f98f7811742
Arian <arian.kulmer@web.de>
2024-11-07 20:54:54 +0100

    sm8150-common: hidl: fingerprint: Eat vendor acquired messages
    UdfpsController is calling tryAodSendFingerUp when it receives
    a vendor acquired message which in turn calls onPointerUp.
    Since our sensors are sending a vendor acquired message during
    scanning, the hal is informed about a finger up event and thus
    disables hbm etc. during scanning in aod/screen off states.
    
    [1]: https://android.googlesource.com/platform/frameworks/base/+/6cc7c9df3aa08ef16efe880fe71fc5c0bf4d21b8/packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java#332
    [2]: https://android.googlesource.com/platform/frameworks/base/+/6cc7c9df3aa08ef16efe880fe71fc5c0bf4d21b8/core/java/android/hardware/biometrics/BiometricFingerprintConstants.java#352
    
    Change-Id: Idffc6582b263b1a1fc0d49322aea4cbf8ff13ec8

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -1,18 +1,8 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
- * Copyright (C) 2018-2020 The LineageOS Project
+ *               2018-2024 The LineageOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.3-service.lenovo_msmnile"
@@ -402,8 +392,10 @@ void BiometricsFingerprint::notify(const fingerprint_msg_t* msg) {
                 ALOGD("onAcquired: Finger up");
             } else {
                 ALOGD("onAcquired(%d)", result);
-                if (!thisPtr->mClientCallback->onAcquired(devId, result, vendorCode).isOk()) {
-                    ALOGE("failed to invoke fingerprint onAcquired callback");
+                if (result != FingerprintAcquiredInfo::ACQUIRED_VENDOR) {
+                    if (!thisPtr->mClientCallback->onAcquired(devId, result, vendorCode).isOk()) {
+                        ALOGE("failed to invoke fingerprint onAcquired callback");
+                    }
                 }
             }
         } break;
