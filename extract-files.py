91ac86ca3778e95e3bde34227a1c56b6455457cf
Marc Bourgoin <themard85@gmail.com>
2024-11-17 17:51:59 -0700

    sm8150-common: Setup ELF checks & regenerate vendor
    According to templates for py extract-utils
    
    https: //github.com/LineageOS/android_tools_extract-utils/tree/lineage-22.0/templates
    Change-Id: Ibb1cef7c9492c091f1626f3c4623ad0cd3e7b708

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,41 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.file import File
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
+
+namespace_imports = [
+    'device/lenovo/sm8150-common',
+    'hardware/qcom-caf/sm8150',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+    ): lib_fixup_vendor_suffix,
+    (
+    ): lib_fixup_remove,
+}
 
 blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
@@ -22,7 +49,8 @@ module = ExtractUtilsModule(
     'sm8150-common',
     'lenovo',
     blob_fixups=blob_fixups,
-    check_elf=False,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
