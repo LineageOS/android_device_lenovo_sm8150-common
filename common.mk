b3dcf61a2a93cd5e8e3984e29beb9ad514bd724b
LuK1337 <priv.luk@gmail.com>
2025-01-31 10:28:03 +0100

    sm8150-common: Move media_codecs_c2.xml to device tree
    Change-Id: Ib2ebfff4281eafd46427180c2666a71d0d144300

--- common.mk
+++ common.mk
@@ -296,6 +296,7 @@ PRODUCT_PACKAGES += \
     libavservices_minijail.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml
     $(LOCAL_PATH)/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
