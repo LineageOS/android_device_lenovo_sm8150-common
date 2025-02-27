864633b97e0faa9ae328d6c77debced26df85e5e
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    sm8150-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b

--- common.mk
+++ common.mk
@@ -223,6 +223,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.lenovo_msmnile
